18aed38174afdb61c6310a35f851340415f5cc0a
wj <bim_wj@sina.com>
2015-07-06 19:27:02 +0800

    初始化项目

---
+++ ShowerIOS/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  ShowerIOS
+//
+//  Created by wangjian on 15/7/6.
+//  Copyright (c) 2015年 wj. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
---
+++ ShowerIOS/ViewController.h
@@ -0,0 +1,15 @@
+//
+//  ViewController.h
+//  ShowerIOS
+//
+//  Created by wangjian on 15/7/6.
+//  Copyright (c) 2015年 wj. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+
+@end
+
